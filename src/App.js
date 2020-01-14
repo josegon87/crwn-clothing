046775fcda1c8aad22cbea349e095524cbf658d0
jogonzalez <jogonzalez@mides.gub.uy>
2020-01-14 17:48:40 -0300

    se implementaron utilidades de firebase, entre esas registro con email

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 
 import HomePage from './pages/homepage/homepage.component';
@@ -21,9 +21,25 @@ class App extends React.Component {
   unsuscribeFromAuth = null
 
   componentDidMount() {
-    this.unsuscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsuscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+
+        console.log(this.state);
+        
+      }
+      else {
+        this.setState({ currentUser: userAuth } );
+      }
     })
   }
 
