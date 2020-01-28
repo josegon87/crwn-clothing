f3bbd7e63c1a6858ea0ab7b90e0fcf238a624baf
José González <eljose007@gmail.com>
2020-01-28 19:10:44 -0300

    se agrega selector

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,24 +2,28 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectCollections } from '../../redux/shop/shop.selectors.js';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors.js';
 
 import './collections-overview.styles.scss';
 import CollectionPreview from '../preview-collection/collection-preview.component';
 
 
-const CollectionsOverview = ({collections}) => (
-    <div className="collections-overview">
-        {
-            collections.map(({id, ...otherCollectionProps }) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))
-        }
-    </div>
-)
+const CollectionsOverview = ({ collections }) => {
+    debugger;
+    console.log(collections);
+    return (
+        <div className="collections-overview">
+            {
+                collections.map(({ id, ...otherCollectionProps }) => (
+                    <CollectionPreview key={id} {...otherCollectionProps} />
+                ))
+            }
+        </div>
+    )
+}
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview);
