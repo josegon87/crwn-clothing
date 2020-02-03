0984d0f48ca48b4c92f249c5a9e96fb5c9ddcd3b
jogonzalez <jogonzalez@mides.gub.uy>
2020-02-03 14:23:19 -0300

    cambios

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -22,7 +22,7 @@ class ShopPage extends React.Component {
         const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
         console.log('hola');
-        collectionRef.onSnapshot(async snapshot => {
+        collectionRef.get().then(snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
             this.setState({ loading: false });
